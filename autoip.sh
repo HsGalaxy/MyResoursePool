4525c5005f054562ddce13f257a060e60e47b71f
HsGalaxy <59781637+HsGalaxy@users.noreply.github.com>
2022-04-17 10:42:26 +0800

    Update autoip.sh

--- autoip.sh
+++ autoip.sh
@@ -1,4 +1,4 @@
 apt update -y
 apt install iptables,python3,screen -y
 wget https://raw.githubusercontent.com/HsGalaxy/MyResoursePool/main/autoIp.py /
-ehco "* * * * * python3 autoIp.py"/var/spool/cron/crontabs
+echo "* * * * * python3 autoIp.py"/var/spool/cron/crontabs
