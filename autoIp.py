b84a06d70672ca59f0657ed75a6039e5fe2ae7d8
HsGalaxy <59781637+HsGalaxy@users.noreply.github.com>
2022-04-17 10:33:17 +0800

    Update autoIp.py

--- autoIp.py
+++ autoIp.py
@@ -1,7 +1,7 @@
 import os
 os.system("curl http://1.116.160.114:15002/getipss -o ips.txt")
 if os.path.exists("aips.txt") != True:
-    os.system("iptables -I INPUT -p tcp --dport 21702 -j DROP")
+    os.system("iptables -I INPUT -p tcp --dport 21703 -j DROP")
     with open("ips.txt", "w") as fx:
         os.system("echo " ">>aips.txt")           
 with open("ips.txt", "r", encoding="utf-8") as fp:
@@ -10,6 +10,6 @@ with open("aips.txt", "r", encoding="utf-8") as fps:
     aips = fps.read().split("\n")
 for ip in ips:
     if ip not in aips:
-        os.system("iptables -I INPUT -s "+ip+" -p tcp --dport "+"21702"+" -j ACCEPT")
+        os.system("iptables -I INPUT -s "+ip+" -p tcp --dport "+"21703"+" -j ACCEPT")
         with open("aips.txt", "a", encoding="utf-8") as fp:
             fp.write(ip+"\n")
