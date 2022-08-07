f9b5cffe523c726e6ff9d406badfa303acc5fe25
HsGalaxy <59781637+HsGalaxy@users.noreply.github.com>
2022-08-07 10:09:28 +0800

    Update autoIp.py

--- autoIp.py
+++ autoIp.py
@@ -1,8 +1,8 @@
 import os
-os.system("curl http://1.116.160.114:15002/getipss -o ips.txt")
+os.system("curl http://45.136.197.234:5001/getipss -o ips.txt")
 if os.path.exists("aips.txt") != True:
-    os.system("iptables -I INPUT -p tcp --dport 21703 -j DROP")
-    os.system("iptables -I INPUT -p udp --dport 21703 -j DROP")
+    os.system("iptables -I INPUT -p tcp --dport 6688 -j DROP")
+    os.system("iptables -I INPUT -p udp --dport 6688 -j DROP")
     with open("ips.txt", "w") as fx:
         os.system("echo " ">>aips.txt")           
 with open("ips.txt", "r", encoding="utf-8") as fp:
@@ -11,7 +11,7 @@ with open("aips.txt", "r", encoding="utf-8") as fps:
     aips = fps.read().split("\n")
 for ip in ips:
     if ip not in aips:
-        os.system("iptables -I INPUT -s "+ip+" -p tcp --dport "+"21703"+" -j ACCEPT")
-        os.system("iptables -I INPUT -s "+ip+" -p udp --dport "+"21703"+" -j ACCEPT")
+        os.system("iptables -I INPUT -s "+ip+" -p tcp --dport "+"6688"+" -j ACCEPT")
+        os.system("iptables -I INPUT -s "+ip+" -p udp --dport "+"6688"+" -j ACCEPT")
         with open("aips.txt", "a", encoding="utf-8") as fp:
             fp.write(ip+"\n")
